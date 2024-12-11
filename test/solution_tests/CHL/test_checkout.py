7073ea35bbae0db649e7a4d62cd3bca58c41677d
sbx_user1051 <sbx_user1051@169.254.6.138>
2024-12-11 22:37:21 +0000

    Wed Dec 11 22:37:21 UTC 2024

--- test/solution_tests/CHL/test_checkout.py
+++ test/solution_tests/CHL/test_checkout.py
@@ -35,4 +35,7 @@ def test_special_offer_e():
 
 def test_combination_offers():
     assert checkout("AAAAAEEB") == 280 # 200 + 80
-    
+
+def test_edge_cases():
+    assert checkout("AAAAAAAA") == 330  # 5A for 200 + 3A for 130
+    assert checkout("EEEEBBB") == 205  # 4E = 160, two B free, pay for one B = 45
